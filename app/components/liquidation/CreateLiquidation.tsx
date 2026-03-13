95c71c2a1131797586e36111de6876a5e64124c4
CodingMantis <tankarlchristian@gmail.com>
2026-03-13 15:34:28 +0800

    feat(nav): navigation for create liquidation page added

---
+++ app/components/liquidation/CreateLiquidation.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function CreateLiquidation() {
+  return (
+    <div>
+      CreateLiquidation
+    </div>
+  )
+}
---
+++ app/home/finance/liquidation/create-liquidation/page.tsx
@@ -0,0 +1,9 @@
+import CreateLiquidation from '@/app/components/liquidation/CreateLiquidation'
+
+export default function CreateLiquidationPage() {
+  return (
+    <div>
+      <CreateLiquidation/>
+    </div>
+  )
+}
